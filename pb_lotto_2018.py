ae37292aa028a03749117443a7fca6186c84cbef
dp0013 <dp0013@users.noreply.github.com>
2018-11-18 21:02:57 +0200

    updated pb_lotto_2018
    changed code to reflect change in dependency module filename and output formatting, results

--- pb_lotto_2018.py
+++ pb_lotto_2018.py
@@ -1,45 +1,26 @@
-<<<<<<< HEAD:pb_lotto_2018.py
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov  6 19:42:49 2018 
 Author: D. Plyler
 
-SUMMARY: This project was conducted to strengthen my current Python skillset while building web scraping familiarity and is not designed to predict any winning lottery combinations.  Rather, it is designed to collect and observe data, looking for any trends out of mere curiosity.  This specific module calls on 'pb_winningNums' to determine the winning powerball lottery numbers from 2009 to 2018 in order of their frequency.  It then prints out the results, the total number of unique winning numbers captured and the date/time the program was executed.
-   
+SUMMARY: This project was conducted to strengthen my current Python skillset while building web scraping familiarity and is not designed to predict any winning lottery combinations.  Rather, it is designed to collect and observe data, looking for any trends out of mere curiosity.  This specific module calls on 'pbWinningNums' to determine the winning powerball lottery numbers from 2009 to 2018 in order of their frequency.  It then prints out the results, the total number of unique winning numbers captured (this should always be 69 as there are only 69 numbers used in the Powerball lottery) and the date/time the program was executed.   
 """
 #imports and executes the module containing the class and supporting functions:
-import pb_winningNums
+import pbWinningNums
 
-pd.set_option('display.max_rows', 100)
-print("This is the list of winning numbers by rank:\n",  (WinningNums.winners_sort()))
-print()
+#ensure these actions only run explicitly, NOT when called by other modules:
+def main():
+    pass
 
-print("Total numbers captured: ", len(WinningNums.winners_sort()))
-print()
-
-now = datetime.datetime.now()
-print("Updated: ", (now.strftime("%Y-%m-%d %H:%M:%S")))
-
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov  6 19:42:49 2018 
-Author: D. Plyler
-
-SUMMARY: This project was conducted to strengthen my current Python skillset while building web scraping familiarity and is not designed to predict any winning lottery combinations.  Rather, it is designed to collect and observe data, looking for any trends out of mere curiosity.  This specific module calls on 'pb_winningNums' to determine the winning powerball lottery numbers from 2009 to 2018 in order of their frequency.  It then prints out the results, the total number of unique winning numbers captured and the date/time the program was executed.
-   
-"""
-#imports and executes the module containing the class and supporting functions:
-import pb_winningNums
-
-pd.set_option('display.max_rows', 100)
-print("This is the list of winning numbers by rank:\n",  (WinningNums.winners_sort()))
-print()
-
-print("Total numbers captured: ", len(WinningNums.winners_sort()))
-print()
-
-now = datetime.datetime.now()
-print("Updated: ", (now.strftime("%Y-%m-%d %H:%M:%S")))
-
->>>>>>> c7e957b2707f8166e834567dafd84d15d5d253ee:pb_lotto_2018.py
+if __name__ == "__main__":
+    
+    pd.set_option('display.max_rows', 100)
+    print("This is the list of winning numbers by rank: ")
+    print()
+    print(WinningNums.winners_sort())
+    print()
+    print("Total numbers captured: ", len(WinningNums.winners_sort()))
+    print()
+    now = datetime.datetime.now()
+    print("Execution completed at ", (now.strftime("%Y-%m-%d %H:%M:%S")))
+    
+    main()

368805358eb6138357c83382b96be1d2f7ffddc8
dp0013 <dp0013@users.noreply.github.com>
2018-12-04 16:00:57 +0200

    Updated pb_lotto_2018.py
    -  removed 'if __name__ == "__main__"' line
    -  added pandas and datetime imports
    -  changed 'import pbWinningNums' to 'from pbWinningNums import WinningNums'

--- pb_lotto_2018.py
+++ pb_lotto_2018.py
@@ -5,14 +5,12 @@ Author: D. Plyler
 SUMMARY: This project was conducted to strengthen my current Python skillset while building web scraping familiarity and is not designed to predict any winning lottery combinations.  Rather, it is designed to collect and observe data, looking for any trends out of mere curiosity.  This specific module calls on 'pbWinningNums' to determine the winning powerball lottery numbers from 2009 to 2018 in order of their frequency.  It then prints out the results, the total number of unique winning numbers captured (this should always be 69 as there are only 69 numbers used in the Powerball lottery) and the date/time the program was executed.   
 """
 #imports and executes the module containing the class and supporting functions:
-import pbWinningNums
+from pbWinningNums import WinningNums
+import pandas as pd
+import datetime
 
 #ensure these actions only run explicitly, NOT when called by other modules:
 def main():
-    pass
-
-if __name__ == "__main__":
-    
     pd.set_option('display.max_rows', 100)
     print("This is the list of winning numbers by rank: ")
     print()
@@ -23,4 +21,4 @@ if __name__ == "__main__":
     now = datetime.datetime.now()
     print("Execution completed at ", (now.strftime("%Y-%m-%d %H:%M:%S")))
     
-    main()
+main()
